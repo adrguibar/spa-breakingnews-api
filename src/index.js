3542e701e4b055b47e9721d7ab4b7dabf30b5571
adriano <adriano.a.barbosa@accenture.com>
2025-04-07 12:07:18 -0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import connectDatabase from './src/database/db.js';
+import connectDatabase from './database/db.js';
 
-import userRouter from './src/routes/user.route.js';
-import authRouter from './src/routes/auth.route.js';
-import newsRouter from './src/routes/news.route.js';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import newsRouter from './routes/news.route.js';
+import swaggerRouter from './routes/swagger.route.js';
 
 dotenv.config();
 
@@ -16,5 +17,6 @@ app.use(express.json());
 app.use("/user", userRouter);
 app.use("/auth", authRouter);
 app.use("/news", newsRouter);
+app.use("/doc", swaggerRouter);
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
